8992201eb4c6c119e59ca3622f2fcf6f3e045d2d
hyeseong <tell280210@gmail.com>
2025-09-02 16:21:46 +0900

    feat: AppShell 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { JoinCompletePage, JoinNicknamePage, LoginPage } from "./pages";
+import AppShell from "./layouts/AppShell";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/join/nickname" element={<JoinNicknamePage />} />
-        <Route path="/join/complete" element={<JoinCompletePage />} />
-      </Routes>
+      <AppShell>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/join/nickname" element={<JoinNicknamePage />} />
+          <Route path="/join/complete" element={<JoinCompletePage />} />
+        </Routes>
+      </AppShell>
     </Router>
   );
 }
