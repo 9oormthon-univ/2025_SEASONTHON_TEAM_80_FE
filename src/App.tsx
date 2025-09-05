dc38944640f3e2086c6a9b3f694d3838078bef16
hyeseong <tell280210@gmail.com>
2025-09-05 09:41:35 +0900

    feat: queryClient 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import AppShell from "./layouts/AppShell";
+import { queryClient } from "./lib/query-client";
 import {
   JoinCompletePage,
   JoinNicknamePage,
@@ -12,8 +13,6 @@ import {
   LoginPage,
   MusicSearchPage,
 } from "./pages";
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
